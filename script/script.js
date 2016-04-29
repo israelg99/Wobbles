9dbc67ea238975d7a67fd691a0f306cf0a3d98c1
Israel Gilyadov <israelg99@gmail.com>
2016-04-29 04:40:25 -0500

    added console logs for debugging

--- script/script.js
+++ script/script.js
@@ -3,17 +3,24 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 
 function preload() {
+	console.log("preload");
+
     loadAssets();
+
+    console.log("finished preload");
 }
 
 function create() {
+	console.log("create");
 
 	createGroups();
 
 	createPlayer();
 
+	console.log("finished create");
 }
 
 function update() {
+	// If we log here, it will spam the whole console, trust me, it works :)
 
 }
--- script/sprite.js
+++ script/sprite.js
@@ -11,20 +11,28 @@ class Sprite {
         this.frameSize = frameSize;
         this.sprite = sprite;
 
-        this.animations = [];
+        this.animations = {};
     }
 }
 
 
 function createGroups() {
+	console.log("creating groups");
+
 	groups.players = game.add.group();
+
+	console.log("finished creating groups");
 }
 
 function createPlayer() {
+	console.log("creating player");
+
 	player = new Sprite(frameSizes["flare"], game.add.sprite(400, 400, 'flare', 1, groups.players));
     player.sprite.scale.set(1.5);
     player.sprite.smoothed = false;
     
-    player.animations.push({"breath":player.sprite.animations.add('breath', [1,2,3,4], 120)});
-    player.animations.push({"walk":player.sprite.animations.add('walk', [5,6,7,8], 60)});
+    player.animations.breath = player.sprite.animations.add('breath', [1,2,3,4], 120);
+    player.animations.walk = player.sprite.animations.add('walk', [5,6,7,8], 60);
+
+    console.log("finished creating player");
 }
