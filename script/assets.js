4f3c89e05bea88c9b7cfc17b276b6f5fed44fce9
Israel Gilyadov <israelg99@gmail.com>
2016-04-30 01:40:59 -0500

    Assets load fixed to the new name

--- script/assets.js
+++ script/assets.js
@@ -23,9 +23,9 @@ function loadCharacters() {
 }
 
 function loadFlare() {
-    game.load.spritesheet('flare', '../assets/sprites/characters/flare/solar_flare_0.png', frameSizes.flare.width, frameSizes.flare.height);
-    game.load.spritesheet('flare', '../assets/sprites/characters/flare/solar_flare_1.png', frameSizes.flare.width, frameSizes.flare.height);
-    game.load.spritesheet('flare', '../assets/sprites/characters/flare/solar_flare_2.png', frameSizes.flare.width, frameSizes.flare.height);
+    game.load.spritesheet('flare', '../assets/sprites/characters/flare/flare_0.png', frameSizes.flare.width, frameSizes.flare.height);
+    game.load.spritesheet('flare', '../assets/sprites/characters/flare/flare_1.png', frameSizes.flare.width, frameSizes.flare.height);
+    game.load.spritesheet('flare', '../assets/sprites/characters/flare/flare_2.png', frameSizes.flare.width, frameSizes.flare.height);
 }
 
 
